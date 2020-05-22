fa1f9bedf8afd40ab937d2adb556f604af894681
Michael Kulinski <kulinski@mit.edu>
2020-05-21 17:19:46 -0700

    Initial Commit

---
+++ MessagingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MessagingApp
+//
+//  Created by Mike Kulinski on 5/21/20.
+//  Copyright © 2020 Mike Kulinski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
